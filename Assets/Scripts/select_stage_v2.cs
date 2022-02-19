7c2100749843b0f7f61ec9148f2c7c3a3082ee77
KHVIII <ytc344@nyu.edu>
2022-02-18 19:05:39 -0500

    Colorlind Text Color + Language Modification

--- Assets/Scripts/select_stage_v2.cs
+++ Assets/Scripts/select_stage_v2.cs
@@ -55,20 +55,20 @@ public class select_stage_v2 : MonoBehaviour
 
     void Awake()
     {
-        pollTitles = new string[] {"Pollution Level:<color=green>Low</color>",
+        pollTitles = new string[] {"Pollution Level:<color=blue>Low</color>",
         "Pollution Level:<color=red>High</color>"};
         pollDescriptions = new string[] {"The fish swims in relatively clean water. Fish swimming requires less effort; therefore, its fitness will decrease slower and it will avoid predators more easily",
         "The fish swims in heavily polluted water. Fitness will drop faster because the fish needs to detoxify. Its movements are heavily restricted and slowed down, making it difficult to avoid predators. Additionally, eggs will be less likely to hatch due to non-ideal water conditions."
         };
 
-        tempTitles = new string[] {"Temp Level:<color=green>Normal</color>",
+        tempTitles = new string[] {"Temp Level:<color=blue>Normal</color>",
         "Temp Level:<color=red>High</color>"};
         tempDescriptions = new string[] {"The fish swims at a normal water temperature. Its fitness will decrease slower.",
         "The fish swims in a higher than optimal temperature. This can cause frenetic swimming and increase anxiety. Fitness will decrease faster and eggs will be less likely to hatch."
         };
 
-        flowTitles = new string[] {"Flow Level:<color=green>Low</color>",
-        "Flow Level:<color=red>High</color>"};
+        flowTitles = new string[] {"Flow Level:<color=blue>Normal</color>",
+        "Flow Level:<color=red>Altered</color>"};
         flowDescriptions = new string[] {"The river has a normal water flow speed.",
         "The river has a higher than normal water flow speed. The fish will need to expend more energy to resist the flow. Its fitness will decrease faster. The increased flow speed also makes it more difficult to maneuver and avoid predators. Eggs will have a lower chance of survival due to the high flow speed."
         };
